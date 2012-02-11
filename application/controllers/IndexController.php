cf7d89d3a25e9b16c17b6b6418c3fc6ade3a55fe
root <root@fedora.stefanpc>
2012-02-11 17:24:19 +0000

    small adjustments to bring application in line with Yaf version

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,8 +5,14 @@ class IndexController extends Zend_Controller_Action
         $page = ($this->_request->getParam('page')) ?: 0;
 
         $blog = new Application_Model_Blog();
+
+
+        /*view*/
         $this->view->entries = $blog->fetchAll();
         $this->view->page = $page;
+
+        /*layout*/
+        $this->view->layout()->meta_title = 'A Blog';
     }
     
 }
